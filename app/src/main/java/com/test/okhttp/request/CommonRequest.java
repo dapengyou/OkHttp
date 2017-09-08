de98a620d030ae9c48ae4b23cec027b7b0b2d0c6
dapengyou <ladyztt@sina.com>
2017-09-08 17:58:38 +0800

    对request进行封装
    对request进行封装

--- app/src/main/java/com/test/okhttp/request/CommonRequest.java
+++ app/src/main/java/com/test/okhttp/request/CommonRequest.java
@@ -1,9 +1,146 @@
 package com.test.okhttp.request;
 
+import java.io.File;
+import java.util.Map;
+
+import okhttp3.FormBody;
+import okhttp3.Headers;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+
 /**
- * 封装所有的请求参数到hasMap中
+ * 接收请求参数，为我们生成Request对象
  * Created by lady_zhou on 2017/9/8.
  */
 
 public class CommonRequest {
+    /**
+     * create the key-value Request
+     *
+     * @param url
+     * @param params
+     * @return 返回一个创建好的request对象
+     */
+    public static Request createPostRequest(String url, RequestParams params) {
+        return createPostRequest(url, params, null);
+    }
+
+    /**
+     * 可以带请求头的Post请求
+     *
+     * @param url
+     * @param params
+     * @param headers
+     * @return  返回一个创建好的request对象
+     */
+    public static Request createPostRequest(String url, RequestParams params, RequestParams headers) {
+        FormBody.Builder mFormBodyBuild = new FormBody.Builder();
+        if (params != null) {
+            for (Map.Entry<String, String> entry : params.urlParams.entrySet()) {
+                //将请求参数便利添加到我们的请求构件类中
+                mFormBodyBuild.add(entry.getKey(), entry.getValue());
+            }
+        }
+        //添加请求头
+        Headers.Builder mHeaderBuild = new Headers.Builder();
+        if (headers != null) {
+            for (Map.Entry<String, String> entry : headers.urlParams.entrySet()) {
+                mHeaderBuild.add(entry.getKey(), entry.getValue());
+            }
+        }
+        //通过请求构件类的build方法获取到真正的请求体对象
+        FormBody mFormBody = mFormBodyBuild.build();
+        Headers mHeader = mHeaderBuild.build();
+        Request request = new Request.Builder().url(url).
+                post(mFormBody).
+                headers(mHeader)
+                .build();
+        return request;
+    }
+
+    /**
+     * ressemble the params to the url
+     *
+     * @param url
+     * @param params
+     * @return
+     */
+    public static Request createGetRequest(String url, RequestParams params) {
+
+        return createGetRequest(url, params, null);
+    }
+
+    /**
+     * 可以带请求头的Get请求
+     *
+     * @param url
+     * @param params
+     * @param headers
+     * @return 通过传入的参数返回一个Get类型的请求
+     */
+    public static Request createGetRequest(String url, RequestParams params, RequestParams headers) {
+        StringBuilder urlBuilder = new StringBuilder(url).append("?");
+        if (params != null) {
+            for (Map.Entry<String, String> entry : params.urlParams.entrySet()) {
+                urlBuilder.append(entry.getKey()).append("=").append(entry.getValue()).append("&");
+            }
+        }
+        //添加请求头
+        Headers.Builder mHeaderBuild = new Headers.Builder();
+        if (headers != null) {
+            for (Map.Entry<String, String> entry : headers.urlParams.entrySet()) {
+                mHeaderBuild.add(entry.getKey(), entry.getValue());
+            }
+        }
+        Headers mHeader = mHeaderBuild.build();
+        return new Request.Builder().
+                url(urlBuilder.substring(0, urlBuilder.length() - 1))
+                .get()
+                .headers(mHeader)
+                .build();
+    }
+
+    /**
+     * @param url
+     * @param params
+     * @return
+     */
+    public static Request createMonitorRequest(String url, RequestParams params) {
+        StringBuilder urlBuilder = new StringBuilder(url).append("&");
+        if (params != null && params.hasParams()) {
+            for (Map.Entry<String, String> entry : params.urlParams.entrySet()) {
+                urlBuilder.append(entry.getKey()).append("=").append(entry.getValue()).append("&");
+            }
+        }
+        return new Request.Builder().url(urlBuilder.substring(0, urlBuilder.length() - 1)).get().build();
+    }
+
+    /**
+     * 文件上传请求
+     *
+     * @return
+     */
+    private static final MediaType FILE_TYPE = MediaType.parse("application/octet-stream");
+
+    public static Request createMultiPostRequest(String url, RequestParams params) {
+
+        MultipartBody.Builder requestBody = new MultipartBody.Builder();
+        requestBody.setType(MultipartBody.FORM);
+        if (params != null) {
+
+            for (Map.Entry<String, Object> entry : params.fileParams.entrySet()) {
+                if (entry.getValue() instanceof File) {
+                    requestBody.addPart(Headers.of("Content-Disposition", "form-data; name=\"" + entry.getKey() + "\""),
+                            RequestBody.create(FILE_TYPE, (File) entry.getValue()));
+                } else if (entry.getValue() instanceof String) {
+
+                    requestBody.addPart(Headers.of("Content-Disposition", "form-data; name=\"" + entry.getKey() + "\""),
+                            RequestBody.create(null, (String) entry.getValue()));
+                }
+            }
+        }
+        return new Request.Builder().url(url).post(requestBody.build()).build();
+    }
 }
